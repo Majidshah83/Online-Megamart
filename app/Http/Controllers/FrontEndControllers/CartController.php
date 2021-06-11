7eb6499b3dc9df6b206649806ee01d6b550807a5
Majidshah83 <shahmajid508@gmail.com>
2021-06-11 11:31:22 +0500

    Create Cartcontroller

--- app/Http/Controllers/FrontEndControllers/CartController.php
+++ app/Http/Controllers/FrontEndControllers/CartController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
-{
+{         //This is Cartcontroller
       public function cartdetail(){
         return view('layouts.Cart');
     }
