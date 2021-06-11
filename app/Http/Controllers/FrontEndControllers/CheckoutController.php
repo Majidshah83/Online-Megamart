164741cd7117937171dd9060958cb8930055406e
Majidshah83 <shahmajid508@gmail.com>
2021-06-11 11:34:17 +0500

    Create Checkout Controller

--- app/Http/Controllers/FrontEndControllers/CheckoutController.php
+++ app/Http/Controllers/FrontEndControllers/CheckoutController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
-{
+{  //This is Checkout Controller
     public function checkoutdetail(){
         return view('layouts.Checkout');
     }
