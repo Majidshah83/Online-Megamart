72a39bc091c045e8c03ffea04ee019ebd1de3c8d
Majidshah83 <shahmajid508@gmail.com>
2021-06-16 12:11:42 +0500

    Property of a Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    //Property of Model
+    protected $connction='mysql';   //for create connection mysql or sqllite
+    protected $table='user'; // For name of table
+    protected $primarykey='prd-id'; // creat primary key
+    protected $keyType='string'; // Non numric and non incremental
+    public $incrementing='false';
+    public $timestamps='false';
 }
